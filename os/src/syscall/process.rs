c86aedc49fd8d22ae55a09e43a1c9da0a56a7516
root <root@DESKTOP-AGCP6AM>
2025-05-11 17:46:52 +0800

    new——f

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -41,5 +41,21 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 // TODO: implement the syscall
 pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match _trace_request {
+        0 =>{
+            let addr = _id as *const u8;
+            return unsafe{*addr as isize};
+        }
+        1 =>{
+            let addr = _id as *mut u8;
+            unsafe {*addr = _data as u8;};
+            return 0;
+        }
+        2 =>{
+            search_counts(_id) as isize
+        }
+        _ => -1
+    }
+
 }
+
